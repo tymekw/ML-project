0b09636bd604cf87e060ba6d61e1491c4d6127fd
tymek <tymwid22@gmail.com>
2021-05-09 15:10:19 +0200

    image from google

--- test.py
+++ test.py
@@ -6,6 +6,7 @@ import urllib3  # the lib that handles the url stuff
 import ast
 import matplotlib.pyplot as plt
 import numpy as np
+import tensorflow as tf
 
 train_ds, metadata = tfds.load(
     'imagenet_v2',
@@ -17,11 +18,14 @@ train_ds, metadata = tfds.load(
 detector = hub.load("https://tfhub.dev/tensorflow/centernet/resnet50v1_fpn_512x512/1")
 
 def predict(my_image):
-    my_image = image.smart_resize(my_image, size=(1024, 1024))
+    # commented for open_image_from_google
+    # my_image = image.smart_resize(my_image, size=(1024, 1024))
     x = image.img_to_array(my_image)
     x = np.expand_dims(x, axis=0)
     preds = detector(x)
     return preds, my_image
+
+
 
 http = urllib3.PoolManager()
 r = http.request('GET', 'https://raw.githubusercontent.com/amikelive/coco-labels/master/coco-labels-paper.txt')
@@ -38,15 +42,33 @@ dane = r.data.decode('UTF-8')
 imagenet = ast.literal_eval(dane)
 print(imagenet)
 
-iterator = iter(train_ds)
-for i in range(0,10):
-    image1, label1 = next(iterator)
-    result, x = predict(image1)
-    plt.figure()
-    plt.imshow(image1)
-    plt.show()
-    print("is: " + imagenet[label1.numpy()])
+# iterator = iter(train_ds)
+# for i in range(0,10):
+#     image1, label1 = next(iterator)
+#     result, x = predict(image1)
+#     plt.figure()
+#     plt.imshow(image1)
+#     plt.show()
+#     print("is: " + imagenet[label1.numpy()])
+#
+#     results = {key:value.numpy() for key,value in result.items()}
+#     detection_classes = result['detection_classes']
+#     print("model thinks: " + coco[detection_classes[0][0].numpy()])
 
+
+
+def open_image_from_google():
+    impath = "D:\\AGH\\VI semestr\\ML\\datasetGoogle\\OIDv4_ToolKit\\OID\\Dataset\\test\\Apple\\964b69442edeec04.jpg"
+
+    from PIL import Image
+    apple = Image.open(impath)
+    # image_tensor = tf.convert_to_tensor(apple)
+    # apple = tf.image.convert_image_dtype(image_tensor,tf.float32)
+    result, x = predict(apple)
     results = {key:value.numpy() for key,value in result.items()}
     detection_classes = result['detection_classes']
     print("model thinks: " + coco[detection_classes[0][0].numpy()])
+
+
+open_image_from_google()
+
