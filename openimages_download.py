c4c2f587bcd647e3bbd9310b549a9b9ce5c289f9
tymek <tymwid22@gmail.com>
2021-05-09 21:18:24 +0200

    labels_dict

--- openimages_download.py
+++ openimages_download.py
@@ -1,6 +1,6 @@
 
 
-def get_dict_all_labels(path = "D:\\AGH\\VI semestr\\ML\\datasetGoogle\\OIDv4_ToolKit\\OID\\csv_folder\\test-annotations-bbox.csv")
+def get_dict_all_labels(path = "D:\\AGH\\VI semestr\\ML\\datasetGoogle\\OIDv4_ToolKit\\OID\\csv_folder\\test-annotations-bbox.csv"):
     import csv
     my_dict = {}
     with open(path, mode='r') as infile:
